f3b6ace370438cd52b10902602f824412f880c8a
Razvan Carp <razvan_ector@yahoo.com>
2024-04-26 12:23:41 +0300

    some completion

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ btn.addEventListener("click", function() {
     //get random number betwen 0 - 3 
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber( {
-    return Math.random()*colors.length;
+    return Math.floor (Math.random() * colors.length);
 
 
 })
